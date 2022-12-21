59210c54bcf24c59eb23cfb3b99fe204ecf756fd
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-12-22 02:48:04 +0700

    Fix negative exponent

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 function powBigInt(base: bigint, exp: bigint): bigint {
+  if (exp < 0) throw new RangeError("Exponent must be positive");
+
   let result: bigint = BigInt(1);
+
   for (let i = 0; i < exp; i++) {
     result *= base;
   }
+
   return result;
 }
 
